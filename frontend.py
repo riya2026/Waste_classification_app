491761079d928656af28d6ae78056ae58fb99055
riya2026 <79354652+riya2026@users.noreply.github.com>
2025-07-01 15:42:25 -0700

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -103,22 +103,7 @@ if language == "English":
             st.image(st.session_state.image, caption="Here is the image you provided", use_column_width=True)
 
             if st.button("🔍 Detect Objects"):
-                # --- CNN Classification first ---
-                #st.subheader("🧠 CNN Classification")
-                #predicted_class = predict_image_class(st.session_state.image, cnnModel)
-                #st.write(f"Predicted Class (CNN): **{predicted_class}**")
-
-                # --- Audio feedback from CNN result ---
-                #if predicted_class in ['apple', 'banana']:
-                #    st.success("This object is **Biodegradable** (based on CNN).")
-                #    st.audio("biodegradable.mp3", format="audio/mp3")
-                #elif predicted_class in ['glass', 'plastic']:
-                #    st.error("This object is **Non-Biodegradable** (based on CNN).")
-                #    st.audio("non-biodegradable.mp3", format="audio/mp3")
-                #else:
-                #    st.warning("Object class could not be determined.")
-
-                # --- YOLO Detection ---
+                
                 st.subheader("YOLOv8 Detection Results")
                 results = model.predict(source=st.session_state.image)
                 result_img = results[0].plot()
